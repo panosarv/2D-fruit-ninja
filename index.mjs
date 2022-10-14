3bb189158111afb9e0fed010df380b05c965acc5
Panagiotis Arvanitis <97358977+panosarv@users.noreply.github.com>
2022-10-14 17:21:08 +0300

    updated background

--- index.mjs
+++ index.mjs
@@ -1,11 +1,138 @@
 
-import { Shaolin,Enivorment,Fruit } from "./js/classes.mjs";
 const canvas=document.querySelector("canvas");
 const context=canvas.getContext("2d")
 const gravity=0.7;
 canvas.width=1024
 canvas.height=576
 
+class Fruit{
+    constructor(){
+        this.position={
+            x:0,
+            y:0
+        }
+        this.velocity=0;
+        this.width=20;
+        this.height=20;
+        this.position.y=-1*Math.floor(Math.random()*5000)+100;
+        this.position.x=Math.floor(Math.random()*canvas.width-this.width)
+        
+        
+    }
+    draw(){
+        context.fillStyle="yellow"
+        context.fillRect(this.position.x,this.position.y,this.width,this.height);
+    }
+    update(){
+        this.draw()
+        if(this.position.y+this.height<0){
+            this.position.y+=1;
+        }
+        else{
+            this.velocity+=0.01;
+            this.position.y+=this.velocity;
+        }
+        
+    }
+    isInside(){
+        if(this.position.y+this.height>0){
+            return true;
+        }
+    }
+}
+
+
+
+class Enviroment{
+    constructor({position,imgSrc}){  
+        this.position=position;
+        this.width=50;
+        this.height=150;
+        this.image=new Image();
+        this.image.src=imgSrc;
+    }
+    draw(){
+        context.drawImage(this.image,this.position.x,this.position.y)
+    }
+    update(){
+        this.draw()
+    }
+}
+
+
+class Shaolin{
+    constructor({position,velocity}){
+        this.position=position;
+        this.velocity=velocity;
+        this.height=150;
+        this.width=50;
+        this.lastKey;
+        this.isAttacking;
+        this.score=0;
+        this.attack={
+            position:{
+                x:this.position.x,
+                y:this.position.y
+            },
+            width:100,
+            height:50
+        }
+    }
+
+    draw(){
+        context.fillStyle="red";
+        context.fillRect(this.position.x,this.position.y,this.width,this.height);
+        if(this.isAttacking){
+            context.fillStyle="green";
+
+            if(this.lastKey=="d"){
+                this.attack.position.x=this.position.x
+                this.attack.position.y=this.position.y
+                context.fillRect(
+                this.attack.position.x,
+                this.attack.position.y,
+                this.attack.width,
+                this.attack.height)
+            }
+            else if(this.lastKey=="a"){
+                this.attack.position.x=this.position.x+50-this.attack.width
+                this.attack.position.y=this.position.y;
+                context.fillRect(
+                    this.attack.position.x,
+                    this.attack.position.y,
+                    this.attack.width,
+                    this.attack.height)
+            }
+            else{
+                this.attack.position.x=this.position.x;
+                context.fillRect(
+                this.attack.position.x,
+                this.attack.position.y,
+                this.attack.width,
+                this.attack.height)
+            }
+        }
+    }
+    update(){
+        this.draw()
+        
+        this.position.x+=this.velocity.x;
+        this.position.y+=this.velocity.y;
+        if(this.height+this.position.y+this.velocity.y>=canvas.height-97){
+            this.velocity.y=0;
+        }
+        else{
+            this.velocity.y+=gravity;
+        }
+    }
+    strike(){
+        this.isAttacking=true;
+        setTimeout(()=>{
+            this.isAttacking=false;
+        },100)
+    }
+
+}
 
 const fruitArray=new Array();
 for(let i=0;i<50;i++){
@@ -13,7 +140,7 @@ for(let i=0;i<50;i++){
     fruitArray.push(fruit);
 }
 
-player=new Shaolin({
+const player=new Shaolin({
     position:{
         x:100,
         y:200
@@ -24,7 +151,21 @@ player=new Shaolin({
     }
     });
 
+const bg=new Enviroment({
+    position:{
+        x:0,
+        y:0
+    },
+    imgSrc:'./assets/background.png'
+})
 
+const shops=new Enviroment({
+    position:{
+        x:0,
+        y:0
+    },
+    imgSrc:'./assets/shop.png'
+})
 
 const keys={
     a:{
@@ -73,6 +214,7 @@ animate=function(){
     window.requestAnimationFrame(animate);
     context.fillStyle="black";
     context.fillRect(0,0,canvas.width,canvas.height)
+    bg.update();
     for(let fruit of fruitArray){
         fruit.update();
         if(fruit.isInside()){
